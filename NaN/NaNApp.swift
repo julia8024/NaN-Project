48ea4255ff75feb8fa209177edc19f4e884d9229
julia8024 <julia8024@naver.com>
2024-10-18 01:06:22 +0900

    Initial Commit

---
+++ NaN/NaNApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaNApp.swift
+//  NaN
+//
+//  Created by 장세희 on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct NaNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
